43755cf4cd5dbbb70461e46205047e4df410e446
JJangQ <95037126+min910@users.noreply.github.com>
2021-12-05 23:07:44 +0900

    Add files via upload

---
+++ syntax/Variable.js
@@ -0,0 +1,7 @@
+
+
+
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
---
+++ syntax/template.js
@@ -0,0 +1,7 @@
+var name = 'JJangQ';
+var letter = `creating ${name}
+web pages and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages.
+
+${name} HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.`;
+
+console.log(letter);
