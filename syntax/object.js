ec69bd515198a82c91886610e1f6731e070dbe4e
min910 <hjm910@hanmail.net>
2021-12-10 00:56:39 +0900

    main

---
+++ syntax/object.js
@@ -0,0 +1,11 @@
+var members = ['test1', 'test2', 'test3'];
+
+console.log(members[1]);
+
+var roles = {
+    'programmer':'test1',
+    'designer' : 'test2',
+    'manager' : 'test3'
+};
+
+console.log(roles.designer);
